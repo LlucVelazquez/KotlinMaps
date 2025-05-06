0cbb02af8e0e739cb235d1c90f433564ae3620bb
lluc.velazquez.7e8 <lluc.velazquez.7e8@itb.cat>
2025-05-06 10:33:01 +0200

    ertre

--- composeApp/src/androidMain/kotlin/cat/itb/damv/m78/MapsScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/damv/m78/MapsScreen.kt
@@ -1,21 +1,44 @@
 package cat.itb.damv.m78
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.AdvancedMarker
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun MapsScreen(){
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState() {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
+    val markers = remember { mutableStateListOf<LatLng>() }
     GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    )
+        googleMapOptionsFactory = {
+            GoogleMapOptions().mapId("DEMO_MAP_ID")
+        },
+        onMapLongClick = { latLng ->
+            markers.add(latLng)
+        }
+    ) {
+        AdvancedMarker(
+            state = MarkerState(position = LatLng(-34.0, 151.0)),
+            title = "Marker in Sydney"
+        )
+        AdvancedMarker(
+            state = MarkerState(position = LatLng(35.66, 139.6)),
+            title = "Marker in Tokyo"
+        )
+        markers.forEach { latLng ->
+            AdvancedMarker(
+                state = MarkerState(position = latLng),
+                title = "Nuevo marcador"
+            )
+        }
+    }
 }
