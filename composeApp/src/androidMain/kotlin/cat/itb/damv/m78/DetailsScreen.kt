b0b9918b9e7b56b50d5b43a85e0ddb8c05e89aef
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-05-13 13:51:34 +0200

    fdhgfddsdsasadsadsda

--- composeApp/src/androidMain/kotlin/cat/itb/damv/m78/DetailsScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/damv/m78/DetailsScreen.kt
@@ -1,24 +1,28 @@
 package cat.itb.damv.m78
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.camera.compose.CameraXViewfinder
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.compose.AsyncImage
 
 @Composable
 fun MarkerDetailScreen(
     markerId: String,
     navViewModel: NavViewModel,
-    markersViewModel: MarkersViewModel = viewModel()
+    markersViewModel: MarkersViewModel = viewModel(),
+    viewModel: Any = viewModel { CameraViewModel() }
+
 ) {
     val marker = markersViewModel.getMarkerById(markerId) ?: return
     var title by remember { mutableStateOf(marker.title) }
@@ -39,6 +43,34 @@ fun MarkerDetailScreen(
             label = { Text("Descripció") }
         )
         Spacer(Modifier.height(20.dp))
+        FeatureThatRequiresCameraPermission() {
+            val viewModel = viewModel { CameraViewModel() }
+            val context = LocalContext.current
+            val lifecycleOwner = LocalLifecycleOwner.current
+            LaunchedEffect(lifecycleOwner) {
+                viewModel.bindToCamera(context.applicationContext, lifecycleOwner)
+            }
+            Box {
+                val photoUrl = viewModel.photoUrl.value
+                if (photoUrl == null) {
+                    val surfaceRequest = viewModel.surferRequest.value
+                    surfaceRequest?.let { request ->
+                        CameraXViewfinder(
+                            surfaceRequest = request,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                    Button(
+                        {viewModel.takePhoto(context) },
+                        modifier = Modifier.align(Alignment.Center)
+                    ) { Text("Take Photo") }
+                } else {
+                    AsyncImage(model = photoUrl, "")
+                }
+            }
+
+        }
+        Spacer(Modifier.height(10.dp))
         Button(onClick = {
             markersViewModel.updateMarker(marker.copy(title = title, description = description))
             navViewModel.navTo(Screen.Markers)
--- composeApp/src/androidMain/kotlin/cat/itb/damv/m78/MapsScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/damv/m78/MapsScreen.kt
@@ -8,16 +8,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.identity.util.UUID
 import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.AdvancedMarker
-import com.google.maps.android.compose.CameraPositionState
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
 import java.util.*
 
 data class MarkerData(
@@ -29,13 +27,13 @@ data class MarkerData(
 
 class MarkersViewModel : ViewModel() {
     val markers = mutableStateListOf<MarkerData>()
-
     init {
         markers.addAll(
             listOf(
                 MarkerData(latLng = LatLng(41.3851, 2.1734), title = "Barcelona"),
                 MarkerData(latLng = LatLng(40.4168, -3.7038), title = "Madrid"),
-                MarkerData(latLng = LatLng(48.8566, 2.3522), title = "París")
+                MarkerData(latLng = LatLng(48.8566, 2.3522), title = "París"),
+                MarkerData(latLng = LatLng(41.45417990295869, 2.1856497659642167), title = "ITB")
             )
         )
     }
@@ -55,16 +53,19 @@ class MarkersViewModel : ViewModel() {
         markers.find { it.id == id }
 }
 
+
+
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun MapsScreen(viewModel: MarkersViewModel = viewModel()){
 
     val cameraPositionState: CameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(LatLng(40.41,-3.70), 5f)
+        position = CameraPosition.fromLatLngZoom(LatLng(41.45417990295869, 2.1856497659642167), 5f)
     }
 
     Box(Modifier.fillMaxSize()) {
         GoogleMap(
+
             googleMapOptionsFactory = {
                 GoogleMapOptions().mapId("DEMO_MAP_ID")
             },
@@ -72,18 +73,27 @@ fun MapsScreen(viewModel: MarkersViewModel = viewModel()){
                 val newMarker = MarkerData(latLng = latLng)
                 viewModel.markers.add(newMarker)
             },
-            cameraPositionState = cameraPositionState
+            cameraPositionState = cameraPositionState,
+
         ) {
+            Circle(
+                center = LatLng(41.45417990295869, 2.1856497659642167),
+                radius = 100.0,
+                strokeColor = Color.Blue,
+                fillColor = Color.Transparent,
+            )
             viewModel.markers.forEach { markerData ->
                 AdvancedMarker(
                     state = MarkerState(position = markerData.latLng),
                     title = markerData.title
                 )
             }
+
         }
+
         Button(
             onClick = {
-                cameraPositionState.position = CameraPosition.fromLatLngZoom(LatLng(40.41,-3.70), 5f)
+                cameraPositionState.position = CameraPosition.fromLatLngZoom(LatLng(41.45417990295869, 2.1856497659642167), 5f)
             }
         ) {
             Text("Tornar")
